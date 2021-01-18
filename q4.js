947a31ba132827ec097a58bf68f5c70708e983c3
ShaiFletcher <77243296+ShaiFletcher@users.noreply.github.com>
2021-01-18 18:46:17 +0200

    Q4

---
+++ q4.js
@@ -0,0 +1,20 @@
+let a = 9;
+let b = 20;
+let c = 3;
+let d = 5;
+let e = 6;
+var max = a;
+if (b > max){
+    max = b;
+}
+if (c > max){
+    max = c;
+}
+if (d > max){
+    max = d;
+}
+if (e > max){
+    max = e;
+}
+console.log(max);
+
