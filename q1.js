8d543745e583eb97c470ba0ff0346bea904d19b5
ShaiFletcher <77243296+ShaiFletcher@users.noreply.github.com>
2021-01-18 17:31:04 +0200

    first upload

---
+++ q1.js
@@ -0,0 +1,9 @@
+var number2 = 2;
+if (number1 > number2){
+    console.log(number1 + " is Bigger");
+}
+else {
+    console.log(number2 + " is Bigger");
+}
+
+var number1 = 9;
---
+++ q2.js
@@ -0,0 +1,10 @@
+let x = 1;
+let y = -3;
+let z = 4;
+var sum = x*z*y;
+if (sum >= 0){
+    alert("sign is +");
+}
+else{
+    alert("sign is -")
+}
